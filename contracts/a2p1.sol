a2e8f956f79c84f7e492ab429187acb8209e65be
Malcolm <malcolm.t129@gmail.com>
2022-11-01 11:28:06 -0400

    Finished

--- contracts/a2p1.sol
+++ contracts/a2p1.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.14;
 contract a2p1 {
     enum EnrollmentType { UNDERGRADUATE, GRADUATE }
     address public owner;
+    uint256 numStudents = 0;
 
     struct Course {
         uint256 number;
@@ -16,33 +17,45 @@ contract a2p1 {
     struct Registered {
         address id;
         uint256 courseNum;
-        uint256 timestamp;
+        uint256 timeStamp;
         uint256 credits;
         bool exists;
     }
 
     struct Student {
         address id;
+        EnrollmentType status;
         uint256 credits;
         bool exists;
+        bool registered;
+        uint256 timeStamp;
+        
     }
 
     mapping (uint256 => Course) public catalogue;
     uint256[] public courses;
 
+    mapping (address => Student) public enrolled;
+    Student[] public allStudents;
+
+    mapping (address => Registered) public registered;
+    Registered[] public allreg;
+
     constructor() {
-        owner = msg.sender;
+        owner = msg.sender;  
     }
 
     function add (uint256 courseNumber, EnrollmentType courseType) public {
-        require(msg.sender ==  owner, "Not authorized to make this transaction");
-        require(!(catalogue[courseNumber].exists), "Class already exists!");
+        require(msg.sender ==  owner, "Not authorized to make this transaction"); //have to be the owner
+        require(catalogue[courseNumber].exists != true, "Class already exists!"); //Class cant exist 
         
         courses.push(courseNumber);
-        Course memory class = catalogue[courseNumber];
+        Course storage class = catalogue[courseNumber];
 
-        class.number = courseNumber;
+        class.number = courseNumber; 
         class.courseType = courseType;
+        class.exists = true;
+        class.numOfStudents = 0;
         
         if (courseNumber == 431){
             class.gradsCanEnroll = false;
@@ -50,18 +63,168 @@ contract a2p1 {
         else {
             class.gradsCanEnroll = true;
         }
+    }
+        //Creates a full class so that the error is produced in registration function
+        function addForFailure (uint256 courseNumber, EnrollmentType courseType) public {
         
+        
+        require(msg.sender ==  owner, "Not authorized to make this transaction"); //have to be the owner
+        require(catalogue[courseNumber].exists != true, "Class already exists!"); //Class cant exist 
+        
+        courses.push(courseNumber);
+        Course storage class = catalogue[courseNumber];
+
+        class.number = courseNumber; 
+        class.courseType = courseType;
         class.exists = true;
-        class.numOfStudents = 0;
+        class.numOfStudents = 30;
+        
+        if (courseNumber == 431){
+            class.gradsCanEnroll = false;
+        }
+        else {
+            class.gradsCanEnroll = true;
+        }
     }
 
-    //function register(uint256 credits, EnrollmentType studentType, uint256 course){}
 
+    //I can probably finesse these paramters with the javascript
+    function register(uint256 _credits, uint256 _course) public returns (address) {
+        address registrant = msg.sender;
+        Student storage student = enrolled[registrant]; 
+        
+        require(enrolled[registrant].exists == true, "This registrant doesn't exist"); 
+        require(catalogue[_course].exists == true, "This class does not exist!");
+        require(catalogue[_course].numOfStudents < 30, "Class is full!");
+        require(enrolled[registrant].registered == false, "Registrant can only register for one class"); //If true can't register for another class.
+        
+        EnrollmentType studentType = enrolled[registrant].status;
+        
+        if (_course == 431){
+            require(studentType != EnrollmentType.GRADUATE, "Not elligible for this class"); //No grad is ellible for this class
+        }
+
+        if (studentType == EnrollmentType.UNDERGRADUATE){
+            require(_course == 431 || _course == 484, "Not elligible for this class"); //Undergrads can only register for these classes
+        } else {
+            require(student.credits >= 20, "Not enough credits to register as a Graduate");
+        }
+
+        catalogue[_course].numOfStudents++; //increase number of students in the class
+        
+        Registered  memory newRegistrant = Registered({
+            id:registrant,
+            courseNum: _course,
+            timeStamp: 0,
+            credits: _credits,
+            exists: true 
+        });
+        
+        allreg.push(newRegistrant);
+        registered[registrant] = newRegistrant;
+        student.registered = true; //they have registered for their one class
+        student.credits += _credits; //add the new credits... dont know if this is the correct thing but it does something with the credits
+        //this is for the time stamp
+
+        return (registrant);
+
+    }
+
+    
+
+    function makeStudent(EnrollmentType _status, uint256 _credits) public {
+        require(msg.sender != owner);
+        Student memory newStudent = Student({
+            id:msg.sender, 
+            status:_status, 
+            credits:_credits, 
+            exists:true,
+            registered: false,
+            timeStamp: block.timestamp
+        });
+        allStudents.push(newStudent); //add new student to the list of all students
+
+        enrolled[msg.sender] = newStudent;
+        numStudents++;
+    }
+
+    function makeStudentForFail(EnrollmentType _status, uint256 _credits) public {
+    require(msg.sender != owner);
+    Student memory newStudent = Student({
+        id:msg.sender, 
+        status:_status, 
+        credits:_credits, 
+        exists:true,
+        registered: false,
+        timeStamp: block.timestamp+ 1 hours
+    });
+    allStudents.push(newStudent); //add new student to the list of all students
+
+    enrolled[msg.sender] = newStudent;
+    numStudents++;
+    }
+
+
+
+    function drop(uint256 course) public {
+        Student memory currStudent = enrolled[msg.sender];
+
+        require(catalogue[course].exists == true, "Course doesn't exist");
+        require(registered[msg.sender].courseNum == course && registered[msg.sender].exists ==true, "Not currently registered to this course");
+        require(block.timestamp < currStudent.timeStamp + 30 minutes, "Too late to drop!");
+        
+        for (uint256 i = 0; i < allreg.length; i++){
+            if (allreg[i].id == currStudent.id) {
+                delete allreg[i];
+                break;
+            }
+        }
+
+        currStudent.registered = false; //turn the registered to false
+        currStudent.timeStamp = 0; //TimeStamp to zero to reset it back to the map default
+
+        
+        //Reset this mapping address to default settings
+        registered[msg.sender].courseNum = 0;
+        registered[msg.sender].timeStamp = 0;
+        registered[msg.sender].credits = 0;
+        registered[msg.sender].exists = false;
+
+
+    }   
+
+    function getRoster(uint256 _course) public {
+
+        require(catalogue[_course].exists == true, "Class doesn't exist");
+       
+        address[] memory classRoster = new address[](numStudents);
+        uint256 j = 0;
+
+       for (uint256 i = 0; i < allreg.length; i++) {
+             
+            if (allreg[i].courseNum == _course){
+                classRoster[j] = allreg[i].id;
+                j++;
+            }
+       }
+
+       emit printRoster(classRoster);
+    }
     
     function showCatalogue() public{
         emit printCatalogue(courses); 
     }
 
+    function printAllStudents() public {
+        emit printAll(allStudents);
+    }
+
+    
+
     event printCatalogue(uint256[] courses);
+
+    event printRoster(address[] classRoster);
+
+    event printAll(Student[] students);
     
 }
--- scripts/a2p2.js
+++ scripts/a2p2.js
@@ -1,143 +1,411 @@
 
 const main = async () => {
   
-   
+  const [owner, randomPerson1, randomPerson2, randomPerson3, randomPerson4, randomPerson5] = await hre.ethers.getSigners();
+  const assignment = await hre.ethers.getContractFactory("a2p1");
+  const assignmentContract = await assignment.deploy();
+  await assignmentContract.deployed();
+  console.log("Contract deployed to: ", assignmentContract.address);
+  console.log("Contract deployed by: ", owner.address);
+  console.log("Current classes that are available are: ")
+  let testAdd;
+  testAdd = await assignmentContract.add(431, 0); 
+  testAdd = await assignmentContract.add(484, 0);
+  testAdd = await assignmentContract.add(617, 1);
+  testAdd = await assignmentContract.add(670, 1);
 
-  /** ==========================================================
-   * Owner adding a course test section.
+  //making students out of all the signers. 
+  let student;
+  student = await assignmentContract.connect(randomPerson1).makeStudent(1,20);
+  await student.wait();
+
+  student = await assignmentContract.connect(randomPerson2).makeStudent(0,10);
+  await student.wait();
+  
+  student = await assignmentContract.connect(randomPerson3).makeStudent(1,15);
+  await student.wait();
+  
+  student = await assignmentContract.connect(randomPerson4).makeStudent(1,25);
+  await student.wait();
+
+  
+  
+  /**===========================================================
+   * Register test section
+   * 
    * 
    * ===========================================================
    */
-    const [owner, randomPerson1, randomPerson2, randomPerson3] = await hre.ethers.getSigners();
-    const assignment = await hre.ethers.getContractFactory("a2p1");
-    const assignmentContract = await assignment.deploy();
-    await assignmentContract.deployed();
-    console.log("Contract deployed to: ", assignmentContract.address);
-    console.log("Contract deployed by: ", owner.address);
-    console.log("The current class available are: ")
+    //return registrationSuccessful();
+
+    //return registrationUnsuccessful1(); //class does not exist failure
+
+    //return registrationUnsuccessful2(); //class is full failure
+
+    //return registrationUNsuccessful3(); //not enough credits as a grad using randomperson3
+
+
+  
+  /** ==========================================================
+   * Owner adding a course test section
+   * 
+   * ===========================================================
+   */
+
+   //return successfulOwnerAdd();
+   //return unsuccessfulOwnerAdd();
+
+
+
+
+
+  /**==============================================================
+   * Getting roster test section
+   * 
+   *===============================================================
+   */
+
+   //return getRosterMoreThan1Student();
+   //return getRosterWtih0Students();
+   //return getRosterFromNonExistent();
+
+   /**================================================================
+    * Drop test section
+    * 
+    * ================================================================
+    */
+
+
+   //return dropSuccessful();
+   //return dropUnsuccessful(); 
+  
+  async function successfulOwnerAdd() {
     
     let testAdd;
-    testAdd = await assignmentContract.add(431, 0); //successful
-    
-    let testAdd2;
-    testAdd2 = await assignmentContract.add(484, 0);
-    
-    let testAdd3;
-    testAdd3 = await assignmentContract.add(617, 1);
-    
-    let testAdd4;
-    testAdd4 = await assignmentContract.add(670, 1);
-
-    
+    testAdd = await assignmentContract.add(432, 0);
     let courses;
     
     courses = await assignmentContract.showCatalogue();
     courseReceipt = await courses.wait();
-    
-    
     
     let arraysize = courseReceipt.events[0].args.courses.length;
     
       for (let i = 0; i < arraysize; i++) {
         console.log(courseReceipt.events[0].args.courses[i].toString());
       } 
-  
-  //unsuccessfulOwnerAdd();
-
-  //testAdd = await assignmentContract.add(431, 1); //Unsucessful
-  
-  //let notOwner;
-  //notOwner = await assignmentContract.connect(randomPerson).add(555, 1); //what happens when youre not the owner
-
-
-  /**
-   * Register test section
-   */
-
-
-  /**
-   * show course catalogue
-   * 
-   * 
-   */
-
-
-  /*async function getCourses(assignmentContract, initialize) {
-    initialize();
-
-    let courses;
-  
-    courses = await assignmentContract.showCatalogue();
-    courseReceipt = await courses.wait();
-    
-    console.log(courseReceipt.events[0].args.courses[1].toString()); //refence!
-    let arraysize = courseReceipt.events[0].args.courses.length;
-  }*/
+  }
 
   
-  
-  
-  
-
-
-  /*async function initialize() {
-    const [owner, randomPerson1, randomPerson2, randomPerson3] = await hre.ethers.getSigners();
-    const assignment = await hre.ethers.getContractFactory("a2p1");
-    const assignmentContract = await assignment.deploy();
-    await assignmentContract.deployed();
-    console.log("Contract deployed to: ", assignmentContract.address);
-    console.log("Contract deployed by: ", owner.address);
-  }*/
-  
-  
-  /*async function successfulOwnerAdd() {
-    const [owner, randomPerson1, randomPerson2, randomPerson3] = await hre.ethers.getSigners();
-    const assignment = await hre.ethers.getContractFactory("a2p1");
-    const assignmentContract = await assignment.deploy();
-    await assignmentContract.deployed();
-    console.log("Contract deployed to: ", assignmentContract.address);
-    console.log("Contract deployed by: ", owner.address);
-    
+  async function unsuccessfulOwnerAdd () {
+ 
     let testAdd;
     testAdd = await assignmentContract.add(431, 0); //successful
-    testAdd = await assignmentContract.add(484, 0);
-    testAdd = await assignmentContract.add(617, 1);
-    testAdd = await assignmentContract.add(670, 1);
 
     let courses;
     
     courses = await assignmentContract.showCatalogue();
     courseReceipt = await courses.wait();
     
-    console.log(courseReceipt.events[0].args.courses[1].toString()); //refence!
     let arraysize = courseReceipt.events[0].args.courses.length;
-      for (let i = 0; i < arraysize-1; i++) {
+    
+      for (let i = 0; i < arraysize; i++) {
         console.log(courseReceipt.events[0].args.courses[i].toString());
       } 
-  }*/
+  }
 
-  
-  /*async function unsuccessfulOwnerAdd (assignmentContract, initialize) {
-    initialize();
-    notOwner = await assignmentContract.connect(randomPerson).add(555, 1); //what happens when youre not the owner
+  async function registrationSuccessful(){
+    
+    let courses;
+    
+    courses = await assignmentContract.showCatalogue();
+    courseReceipt = await courses.wait();
+    
+    let arraysize = courseReceipt.events[0].args.courses.length;
+    
+      for (let i = 0; i < arraysize; i++) {
+        console.log(courseReceipt.events[0].args.courses[i].toString());
+      } 
+    
+    let student;
+    student = await assignmentContract.connect(randomPerson1).register(3,484);
+    studentTx = await student.wait();
 
-  }*/
-  
-  
-  /*async function printCourses () {
-    initialize();
+    res = await assignmentContract.getRoster(484);
+    confirmation = await res.wait();
+
+    arraysize = confirmation.events[0].args[0].length;
+    console.log("Current students enrolled in this class are: \n ")
+    
+    
+    //filter out the non students in registration
+    for (let i  = 0; i < arraysize; i++){
+      if (confirmation.events[0].args[0][i] != "0x0000000000000000000000000000000000000000"){
+        console.log(confirmation.events[0].args[0][i]);
+      }
+    }
+    
+  }
+
+  async function registrationUnsuccessful1(){
+    let courses;
+    
+    courses = await assignmentContract.showCatalogue();
+    courseReceipt = await courses.wait();
+    
+    let arraysize = courseReceipt.events[0].args.courses.length;
+    
+      for (let i = 0; i < arraysize; i++) {
+        console.log(courseReceipt.events[0].args.courses[i].toString());
+      }
       
+    
+    let student;
+    student = await assignmentContract.connect(randomPerson1).register(3,451);
+    studentTx = await student.wait();
+  }
+
+  async function registrationUnsuccessful2(){
+    
+    let testAdd;
+    testAdd = await assignmentContract.addForFailure(432, 0);
+    
+    
     let courses;
     
     courses = await assignmentContract.showCatalogue();
     courseReceipt = await courses.wait();
     
-    console.log(courseReceipt.events[0].args.courses[1].toString()); //refence!
     let arraysize = courseReceipt.events[0].args.courses.length;
-      for (let i = 0; i < arraysize-1; i++) {
+    
+      for (let i = 0; i < arraysize; i++) {
+        console.log(courseReceipt.events[0].args.courses[i].toString());
+      }
+      
+    
+    let student;
+    student = await assignmentContract.connect(randomPerson1).register(3,432);
+    studentTx = await student.wait();
+  }
+
+  async function registrationUNsuccessful3(){
+    
+    let courses;
+    
+    courses = await assignmentContract.showCatalogue();
+    courseReceipt = await courses.wait();
+    
+    let arraysize = courseReceipt.events[0].args.courses.length;
+    
+      for (let i = 0; i < arraysize; i++) {
         console.log(courseReceipt.events[0].args.courses[i].toString());
       } 
-  }*/
+    
+    let student;
+    student = await assignmentContract.connect(randomPerson3).register(3,484);
+    studentTx = await student.wait();
+
+    res = await assignmentContract.getRoster(484);
+    confirmation = await res.wait();
+
+    arraysize = confirmation.events[0].args[0].length;
+    console.log("Current students enrolled in this class are: \n ")
+    
+    
+    //filter out the non students in registration
+    for (let i  = 0; i < arraysize; i++){
+      if (confirmation.events[0].args[0][i] != "0x0000000000000000000000000000000000000000"){
+        console.log(confirmation.events[0].args[0][i]);
+      }
+    }
+    
+  }
+
+  async function getRosterMoreThan1Student(){
+        
+    let courses;
+    
+    courses = await assignmentContract.showCatalogue();
+    courseReceipt = await courses.wait();
+    
+    let arraysize = courseReceipt.events[0].args.courses.length;
+    
+      for (let i = 0; i < arraysize; i++) {
+        console.log(courseReceipt.events[0].args.courses[i].toString());
+      } 
+    
+    let student;
+    student = await assignmentContract.connect(randomPerson1).register(3,484);
+    studentTx = await student.wait();
+
+    student = await assignmentContract.connect(randomPerson4).register(3,484);
+    studentTx = await student.wait();
+
+    res = await assignmentContract.getRoster(484);
+    confirmation = await res.wait();
+
+    arraysize = confirmation.events[0].args[0].length;
+    console.log("Current students enrolled in this class are: \n ")
+    
+    
+    //filter out the non students in registration
+    for (let i  = 0; i < arraysize; i++){
+      if (confirmation.events[0].args[0][i] != "0x0000000000000000000000000000000000000000"){
+        console.log(confirmation.events[0].args[0][i]);
+      }
+    }
+  }
+
+  async function getRosterWtih0Students(){
+        
+    let courses;
+    
+    courses = await assignmentContract.showCatalogue();
+    courseReceipt = await courses.wait();
+    
+    let arraysize = courseReceipt.events[0].args.courses.length;
+    
+      for (let i = 0; i < arraysize; i++) {
+        console.log(courseReceipt.events[0].args.courses[i].toString());
+      }
+
+    res = await assignmentContract.getRoster(484);
+    confirmation = await res.wait();
+
+    arraysize = confirmation.events[0].args[0].length;
+    console.log("Current students enrolled in this class are: \n ")
+    
+    
+    //filter out the non students in registration
+    for (let i  = 0; i < arraysize; i++){
+      if (confirmation.events[0].args[0][i] != "0x0000000000000000000000000000000000000000"){
+        console.log(confirmation.events[0].args[0][i]);
+      }
+    }
+  }
+
+  async function getRosterFromNonExistent(){
+        
+    let courses;
+    
+    courses = await assignmentContract.showCatalogue();
+    courseReceipt = await courses.wait();
+    
+    let arraysize = courseReceipt.events[0].args.courses.length;
+    
+      for (let i = 0; i < arraysize; i++) {
+        console.log(courseReceipt.events[0].args.courses[i].toString());
+      }
+
+    res = await assignmentContract.getRoster(451);
+    confirmation = await res.wait();
+
+    arraysize = confirmation.events[0].args[0].length;
+    console.log("Current students enrolled in this class are: \n ")
+    
+    
+    //filter out the non students in registration
+    for (let i  = 0; i < arraysize; i++){
+      if (confirmation.events[0].args[0][i] != "0x0000000000000000000000000000000000000000"){
+        console.log(confirmation.events[0].args[0][i]);
+      }
+    }
+  }
+  
+    async function dropSuccessful(){
+    
+    let courses;
+    
+    courses = await assignmentContract.showCatalogue();
+    courseReceipt = await courses.wait();
+    
+    let arraysize = courseReceipt.events[0].args.courses.length;
+    
+      for (let i = 0; i < arraysize; i++) {
+        console.log(courseReceipt.events[0].args.courses[i].toString());
+      } 
+    
+    let student;
+    student = await assignmentContract.connect(randomPerson1).register(3,484); //this would put the student in the roster
+    studentTx = await student.wait();
+
+    student = await assignmentContract.connect(randomPerson1).drop(484); //this will make an empty roster pop up
+    studentTx = await student.wait();
+    
+
+    res = await assignmentContract.getRoster(484);
+    confirmation = await res.wait();
+
+    arraysize = confirmation.events[0].args[0].length;
+    console.log("Current students enrolled in this class are: \n ")
+    
+    
+    //filter out the non students in registration
+    for (let i  = 0; i < arraysize; i++){
+      if (confirmation.events[0].args[0][i] != "0x0000000000000000000000000000000000000000"){
+        console.log(confirmation.events[0].args[0][i]);
+      }
+    }
+    
+  }
+
+  async function dropUnsuccessful(){
+    
+    let courses;
+    
+    courses = await assignmentContract.showCatalogue();
+    courseReceipt = await courses.wait();
+    
+    let arraysize = courseReceipt.events[0].args.courses.length;
+    
+      for (let i = 0; i < arraysize; i++) {
+        console.log(courseReceipt.events[0].args.courses[i].toString());
+      } 
+    
+
+      let student;
+    student = await assignmentContract.connect(randomPerson1).makeStudentForFail(1,20);
+    await student.wait();
+    
+    student = await assignmentContract.connect(randomPerson1).register(3,484); //this would put the student in the roster
+    studentTx = await student.wait();
+
+    student = await assignmentContract.connect(randomPerson1).drop(484); //this will make an empty roster pop up
+    studentTx = await student.wait();
+    
+
+    res = await assignmentContract.getRoster(484);
+    confirmation = await res.wait();
+
+    arraysize = confirmation.events[0].args[0].length;
+    console.log("Current students enrolled in this class are: \n ")
+    
+    
+    //filter out the non students in registration
+    for (let i  = 0; i < arraysize; i++){
+      if (confirmation.events[0].args[0][i] != "0x0000000000000000000000000000000000000000"){
+        console.log(confirmation.events[0].args[0][i]);
+      }
+    }
+    
+  }
+  
+  async function showRoster(){    
+    let courses;
+    courses = await assignmentContract.showCatalogue();
+    courseReceipt = await courses.wait();
+
+    let arraysize = courseReceipt.events[0].args.courses.length;
+    
+      for (let i = 0; i < arraysize; i++) {
+        console.log(courseReceipt.events[0].args.courses[i].toString());
+      }
+
+    reg = await assignmentContract.printAllStudents();
+    confirmation = await reg.wait();
+    
+    //console.log(confirmation.events[0].args);
+
+  }
 
   
   };
